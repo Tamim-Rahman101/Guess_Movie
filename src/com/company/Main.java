8c7900480f340059e2cd686a9b4b5f72c84b1ad6
Tamim Rahman <s2010176137@ru.ac.bd>
2021-08-27 20:18:05 +0600

    n

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("movieList.txt");
+        Scanner movieScan = new Scanner(file);
+
+        int movieListCount = 0;       //to count movie numbers
+        String[] movieName = new  String[100];    //to get movies name into a string
+        while (movieScan.hasNextLine()){
+            movieListCount++;
+            movieName[movieListCount] = movieScan.nextLine();
+        }
+        int movieIndex =(int)(Math.random()*movieListCount+1);  //generate random number to pick random movie
+        System.out.println(movieListCount+"    "+movieIndex+"   "+movieName[movieIndex]);
+
+        int movieLength = movieName[movieIndex].length();   //length of the random movie
+
+        String myMovie = "";   //string only contains "_____..."
+        String wrongLetters = "";
+        int i,chance = 0,point = 0,spaceCount = 0;
+        boolean ok;
+        for(i=0;i<movieLength;i++)
+        {
+            if(movieName[movieIndex].charAt(i) == ' ')
+            {
+                myMovie += ' ';
+                spaceCount++;
+            }
+            else
+                myMovie += '_';
+        }
+
+        point = spaceCount;
+        Scanner inputScan = new Scanner(System.in);
+        char input;
+
+        while (chance <10 && point < movieLength){
+            System.out.println("You are guessing : "+myMovie);
+            System.out.println("You have guessed ("+chance+") wrong letters :"+wrongLetters);
+            System.out.print("Guess a letter : ");
+            ok = false;
+            input = inputScan.next().charAt(0);
+            //System.out.println(input);
+            for(i=0;i<movieLength; i++) {
+                if (movieName[movieIndex].charAt(i) == input) {
+                    ok = true;
+                    point++;
+                    myMovie = myMovie.substring(0,i)+input+myMovie.substring(i+1,movieLength);
+                }
+            }
+            if(!ok)
+            {
+                chance++;
+                wrongLetters += (" " + input);
+            }
+            //System.out.println("                            "+myMovie+"   c "+chance+"   p "+point+"  l "+movieLength);
+        }
+        if(chance < 10)
+            System.out.println("You Win!\nYou have guessed '"+myMovie+"' correctly.");
+        else
+            System.out.println("You Loss!");
+    }
+}
